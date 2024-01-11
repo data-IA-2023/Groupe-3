ac7a0387e14bfa28bd1bd774983c91e9250a665f
COMEWELCOME <mohasoft@gmail.com>
2024-01-11 17:02:52 +0100

    test

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
 import pandas as pd
+from sklearn import datasets
 
+# Charger le jeu de données Iris
+iris = datasets.load_iris()
 
-df = pd.DataFrame({
-    'A': ['John', 'Boby', 'Mina'],
-    'B': ['Masters', 'Graduate', 'Graduate'],
-    'C': [27, 23, 21]
-})
+# Créer un DataFrame à partir du jeu de données Iris
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
 
-df.pivot('A', 'B', 'C')
+# Ajouter les cibles (espèces d'iris) au DataFrame
+df['target'] = iris.target
+
+# Mapper les cibles aux noms des espèces d'iris
+target_names = {0: 'setosa', 1: 'versicolor', 2: 'virginica'}
+df['target_names'] = df['target'].map(target_names)
+
+# Afficher les premières lignes du DataFrame
+print(df.head())
